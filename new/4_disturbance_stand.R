2deae61705e32704e9c89c58f52b265de987e245
ondrej-vostarek <vostarek.ondrej@gmail.com>
2024-04-20 16:18:38 +0200

    version update: disturbance_stand

--- new/4_disturbance_stand.R
+++ new/4_disturbance_stand.R
@@ -1,15 +1,15 @@
 # 0. setup ----------------------------------------------------------------
 
-# R 3.6.3 (2020-02-29)
+# R 4.2.3 (2023-03-15) "Shortstop Beagle"
 
-# data.table 1.12.8
-library(pool) # 0.1.4.3
-library(tidyverse) # 1.3.0 (dplyr 1.0.7, forcats 0.5.0, ggplot2 3.3.5, purr 0.3.4, readr 1.3.1, stringr 1.4.0, tibble 3.0.0, tidyr 1.0.2)
-library(pracma) # 2.2.9
-library(RPostgreSQL) # 0.6-2 (DBI 1.1.0)
-library(sf) # 0.9-5 (GEOS 3.8.0, GDAL 3.0.4, PROJ 6.3.1)
-library(sp)
-library(zoo) # 1.8-7
+# data.table 1.15.2
+library(pool) # 1.0.3
+library(tidyverse) # 2.0.0 (dplyr 1.1.4, forcats 1.0.0, ggplot2 3.5.0, lubridate 1.9.3, purrr 1.0.2, readr 2.1.5, stringr 1.5.1, tibble 3.2.1, tidyr 1.3.1)
+library(pracma) # 2.4.4
+library(RPostgreSQL) # 0.7-6 (DBI 1.2.2)
+library(sf) # 1.0-15 (GEOS 3.9.3, GDAL 3.5.2, PROJ 8.2.1)
+library(sp) # 2.1-3
+library(zoo) # 1.8-12
 
 source("new/pw.R")
 
@@ -33,73 +33,72 @@ dist.events <- tbl(KELuser, "dist_event") %>%
   
 # 4. 2. bootstrapping -----------------------------------------------------
 
-boot <- dist.events %>%
-  distinct(., stand, plotid) %>%
-  slice(rep(1:nrow(.), each = 1000)) %>%
-  mutate(rep = rep(1:1000, times = nrow(.) / 1000)) 
-
 set.seed(1)
 
-dist.boot <- boot %>%
+dist.boot <- dist.events %>%
+  distinct(., stand, plotid) %>%
+  slice(rep(1:nrow(.), each = 1000)) %>%
+  mutate(rep = rep(1:1000, times = nrow(.) / 1000)) %>%
   group_by(stand, rep) %>%
   slice_sample(., n = 10, replace = TRUE) %>%
   ungroup() %>%
-  left_join(., dist.events, by = c("stand", "plotid")) %>%
-  do({
-    
-    x <- .
-    
-    inner_join(
-      x %>% 
-        filter(!is.na(year)) %>%
-        group_by(stand, rep, year) %>%
-        summarise(freq = n() / first(nplots)) %>%
-        ungroup(),
-      x %>% 
-        group_by(stand, rep, plotid) %>% 
-        filter(year %in% first(year)) %>% 
-        group_by(stand, rep) %>% 
-        summarise(year_min = max(year_min),
-                  year_max = min(year_max)) %>%
-        group_by(stand) %>%
-        summarise(year_min = round(mean(year_min), 0),
-                  year_max = round(mean(year_max), 0)) %>%
-        ungroup(),
-      by = "stand")
-    
-  }) %>%
-  filter(year >= year_min, year <= year_max) %>%
-  group_by(stand, rep) %>%
-  complete(year = (min(year_min)-30):(max(year_max)+30), fill = list(freq = 0)) %>%
-  mutate(density_pre = kdeFun(freq, k = 30, bw = 5, st = 7),
-         density = rollapply(density_pre, width = 5, FUN = mean, fill = 0)) %>% 
-  filter(year %in% c((min(year)+15):(max(year)-15))) %>%
-  group_by(stand) %>%
-  mutate(year_min = first(year_min[!is.na(year_min)]), 
-         year_max = first(year_max[!is.na(year_max)])) %>%
-  ungroup()
-
-# write.table(dist.boot, "dist_boot.txt", sep = ",", row.names = F, na = "")
-# dist.boot <- read.table("dist_boot.txt", sep = ",", header = T, stringsAsFactors = F)
+  left_join(., dist.events, by = c("stand", "plotid"))
 
 # 4. 3. peak detection ----------------------------------------------------
 
-dist.peaks <- dist.boot %>%
-  group_by(stand, rep) %>%
-  filter(row_number() %in% peakDetection(x = density, threshold = 0.00001, nups = 5, mindist = 10),
-         year %in% c((min(year_min)+1):(max(year_max)-1))) %>%
-  group_by(stand, year_min, year_max, year) %>%
-  summarise(freq = n() / 100) %>%
-  group_by(stand) %>%
-  complete(year = (min(year_min)-10):(max(year_max)+10), fill = list(freq = 0)) %>%
-  mutate(freqsmooth = kdeFun(freq, k = 11, bw = 1, st = 7)/10) %>%
-  filter(row_number() %in% peakDetection(x = freqsmooth, threshold = 0.00001, nups = 0, mindist = 10)) %>%
-  ungroup() %>%
-  select(stand, year_min, year_max, peakyear = year)
+dist.peaks <- tibble()  
+  
+for (s in unique(dist.boot$stand)) {
+  
+  x <- dist.boot %>% filter(stand %in% s)
+
+  out <- inner_join(
+    x %>% 
+      filter(!is.na(year)) %>%
+      group_by(stand, rep, year) %>%
+      summarise(freq = n() / first(nplots)) %>%
+      ungroup(),
+    x %>% 
+      group_by(stand, rep, plotid) %>% 
+      filter(year %in% first(year)) %>% 
+      group_by(stand, rep) %>% 
+      summarise(year_min = max(year_min),
+                year_max = min(year_max)) %>%
+      group_by(stand) %>%
+      summarise(year_min = round(mean(year_min), 0),
+                year_max = round(mean(year_max), 0)),
+    by = "stand") %>%
+    filter(year >= year_min, year <= year_max) %>%
+    group_by(stand, rep) %>%
+    complete(year = (min(year_min)-30):(max(year_max)+30), fill = list(freq = 0)) %>%
+    mutate(density_pre = kdeFun(freq, k = 30, bw = 5, st = 7),
+           density = rollapply(density_pre, width = 5, FUN = mean, fill = 0)) %>%
+    filter(year %in% c((min(year)+15):(max(year)-15))) %>%
+    group_by(stand) %>%
+    mutate(year_min = first(year_min[!is.na(year_min)]), 
+           year_max = first(year_max[!is.na(year_max)])) %>%
+    group_by(stand, rep) %>%
+    filter(row_number() %in% peakDetection(x = density, threshold = 0.00001, nups = 5, mindist = 10),
+           year %in% c((min(year_min)+1):(max(year_max)-1))) %>%
+    group_by(stand, year_min, year_max, year) %>%
+    summarise(freq = n() / 100) %>%
+    group_by(stand) %>%
+    complete(year = (min(year_min)-10):(max(year_max)+10), fill = list(freq = 0)) %>%
+    mutate(freqsmooth = kdeFun(freq, k = 11, bw = 1, st = 7)/10) %>%
+    filter(row_number() %in% peakDetection(x = freqsmooth, threshold = 0.00001, nups = 0, mindist = 10)) %>%
+    ungroup() %>%
+    select(stand, year_min, year_max, peakyear = year)
+  
+    dist.peaks <- bind_rows(dist.peaks, out)
+    
+    print(paste(s, Sys.time()))
+    
+    remove(s, x, out)
+}
 
 # write.table(dist.peaks, "dist_peaks.csv", sep = ",", row.names = F, na = "")
 # dist.peaks <- read.table("dist_peaks.csv", sep = ",", header = T, stringsAsFactors = F)
-
+  
 # 4. 4. join plot-level events to stand-level peaks -----------------------
 
 dist.events.dt <- data.table::data.table(
@@ -115,9 +114,6 @@ dist.events.joined <-  data.frame(
   dist.peaks.dt[dist.events.dt,
                 list(stand, nplots, year_min, year_max, peakyear, year, plot_id, plotid),
                 roll = "nearest"])
-
-# write.table(dist.events.joined, "dist_events_joined.csv", sep = ",", row.names = F, na = "")
-# dist.events.joined <- read.table("dist_events_joined.csv", sep = ",", header = T, stringsAsFactors = F)
 
 # 4. 5. patch area & stand size -------------------------------------------
 
@@ -142,30 +138,30 @@ for (s in unique(polygons$stand)) {
     
     for (i in 1:length(x[[2]][[1]])) {
       
-      if(length(x[[2]][[1]][[i]]) > 1){
+      if((length(x[[2]][[1]]) != 1) & (length(x[[2]][[1]][[i]])) > 1){
         
-        print(paste(s,p,i, sep = "_"))
+        a <- Polygon(x[[2]][[1]][[i]][[1]])
+        b <- Polygon(x[[2]][[1]][[i]][[2]])
+
+        if(a@hole == T){
+
+          print(paste(s, p, i, 1, "hole", sep = "_"))
+
+          y <- b
+
+        } else {
+
+          print(paste(s, p, i, 2, "hole", sep = "_"))
+
+          y <- a
+
+        }
+
+        y <- Polygons(list(y), i); y <- SpatialPolygons(list(y))
+
+        l[[i]] <- y
         
-        # a <- Polygon(x[[2]][[1]][[i]][[1]])
-        # b <- Polygon(x[[2]][[1]][[i]][[2]])
-        # 
-        # if(a@hole == T){
-        #   
-        #   print(paste(s,p,i,1, sep = "_"))
-        #   
-        #   y <- b
-        #   
-        # } else {
-        #   
-        #   print(paste(s,p,i,2, sep = "_"))
-        #   
-        #   y <- a
-        #   
-        # }
-        # 
-        # y <- Polygons(list(y), i); y <- SpatialPolygons(list(y))
-        # 
-        # l[[i]] <- y
+        remove(a, b)
 
         } else {
         
@@ -173,7 +169,7 @@ for (s in unique(polygons$stand)) {
         
         if(y@hole == T) {
           
-          print(paste(s,p,i, sep = "_"))
+          print(paste(s, p, i, "hole", sep = "_"))
           
         } else {
           
@@ -212,15 +208,13 @@ for (s in unique(polygons$stand)) {
 dist.patches <- patches %>%
   group_by(stand, peakyear, npatch) %>% 
   mutate(nplots_dist = n()) %>%
+  ungroup() %>%
   inner_join(., as.data.frame(polygons) %>% 
               distinct(., stand, plotid, area) %>% 
               group_by(stand) %>% 
               summarise(stand_size = sum(area)),
             by = "stand") %>%
   select(stand, stand_size, peakyear, npatch, patch_area, nplots_dist, plot_id)
-
-# write.table(dist.patches, "dist_patches.csv", sep = ",", row.names = F, na = "")
-# dist.patches <- read.table("dist_patches.csv", sep = ",", header = T, stringsAsFactors = F)
 
 # 4. 6. proportion of plots disturbed & finalisation ----------------------
 
